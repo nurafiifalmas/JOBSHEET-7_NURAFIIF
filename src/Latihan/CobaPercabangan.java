30345bb12e65c3968df5b2396d5a4bc2e757c83c
user <user@LAPTOP-47AR98S0>
2018-08-03 08:56:10 +0700

    Initial Commit

---
+++ src/Latihan/CobaPercabangan.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author user
+ */
+public class CobaPercabangan {
+    public static void main(String[] args) {
+        String identitas = "Nurafiif Almas Azhari / X RPL 5 / 33";
+        System.out.println("Identitas : " +identitas );
+        
+
+    }
+}
