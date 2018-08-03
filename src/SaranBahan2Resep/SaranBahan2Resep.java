5e5f25dc4cb3c5de7d515666623e14ef29f1621c
user <user@LAPTOP-47AR98S0>
2018-08-03 13:06:39 +0700

    Initial Commit

---
+++ src/SaranBahan2Resep/SaranBahan2Resep.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SaranBahan2Resep;
+
+/**
+ *
+ * @author user
+ */
+public class SaranBahan2Resep {
+    public static void main(String[] args) {
+        String identitas = "Nurafiif Almas Azhari / X RPL 5/ 33";
+        System.out.println("Identitas :"+ identitas);
+       
+        System.out.print("\nSaran Resep dari Bahan Milik Anda\n");
+        
+
+    }
+              
+}
