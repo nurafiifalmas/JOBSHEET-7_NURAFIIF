b847abac928a66cf405f8a6857a2db801d0c0c51
user <user@LAPTOP-47AR98S0>
2018-08-03 12:49:18 +0700

    Coba SWITCH..CASE

--- src/CobaPercabangan/CobaPercabangan.java
+++ src/CobaPercabangan/CobaPercabangan.java
@@ -67,8 +67,17 @@ public class CobaPercabangan {
                System.out.println("nilai a lebih dari 2");
          if (a < 4)
                 System.out.println("nilai a kurang dari 4");
+     }
+       System.out.println("\nSWITCH..CASE");
+    switch(a) {
+        case 1: System.out.println("nilai a => 1"); break;
+        case 2: System.out.println("nilai a => 2"); break;
+        case 3: 
+        case 4: System.out.println("nilai a => 3 atau 4"); break;
+        case 5: System.out.println("nilai a => 5"); break;
+        default: System.out.println("nilai a bukan antara 1-5");
+        
 
-   
          
     }
     
@@ -76,6 +85,8 @@ public class CobaPercabangan {
     }
     
 }
+
+    
     
 
     
